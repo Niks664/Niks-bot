6e6eea6bb8c4d766e3dd93e2e821b648518a345b
Niks664 <nikitasergeew123@gmail.com>
2023-09-16 19:56:07 +0300

    123

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-import config
+﻿import config
 import logging
-import asyncio
 from aiogram import Bot,Dispatcher,types, executor
 from filters import IsAdminFilter
 from db import Database
@@ -21,7 +20,7 @@ async def cmd_mute(message: types.Message):
     mute_min = int(message.text[6:])
     db.add_mute(message.reply_to_message.from_user.id, mute_min)
     await message.bot.delete_message(config.GROUP_ID, message.message_id)
-    await message.reply_to_message.reply(f"Пользователь был замучен на {mute_min} минут! \nПомолчи пж :)")
+    await message.reply_to_message.reply(f"Пользователь был замучен на {mute_min} секунд! \nПомолчи пж :)")
     
 @kd.message_handler(is_admin=True, commands = ["ban"], commands_prefix = '!/')
 async def cmd_ban(message: types.Message):
@@ -36,20 +35,17 @@ async def cmd_ban(message: types.Message):
 async def start_command(message: types.Message):
     await message.reply("Привет! \nСкоро тут будет инфа :)")
     
+        
 @kd.message_handler()
 async def filter_messages(message: types.Message):    
     if "Проверка" in message.text:
         await message.delete()
-
-@kd.message_handler()
-async def mess_handler(message: types.Message):
     if not db.user_exists(message.from_user.id):
         db.add_user(message.from_user.id)
     if not db.mute(message.from_user.id):
-        print("g")
+        print("/")
     else:
         await message.delete()
-
 
 if __name__ == "__main__":
     executor.start_polling(kd, skip_updates=True)
--- db.py
+++ db.py
@@ -1,27 +1,26 @@
 import sqlite3
 import time
-from singleton import Singleton
 
-class Database(Singleton):
+class Database:
     def __init__(self, db_file):
         self.connection = sqlite3.connect(db_file)
         self.cursor = self.connection.cursor()
         
     def user_exists(self, user_id):
         with self.connection:
-            result = self.cursor.execute("SELECT * FROM 'user' WHERE 'user_id' =?", (user_id,)).fetchall()
+            result = self.cursor.execute("select * from 'users' where 'user_id' =?", (user_id,)).fetchall()
             return bool(len(result))
-        
+            
     def add_user(self, user_id):
         with self.connection:
-            return self.connection.execute("REPLACE INTO 'user' ('user_id') VALUES (?)", (user_id,))
+            return self.connection.execute("INSERT INTO 'users' ('user_id') VALUES (?)", (user_id,))
         
     def mute(self, user_id):
         with self.connection:
-            user = self.cursor.execute("SELECT * FROM 'user' WHERE 'user_id' = ?", (user_id,)).fetchone()
+            user = self.connection.execute("SELECT * FROM 'users' WHERE 'user_id' = ?", (user_id,)).fetchone()
             return int(user[2]) >= int(time.time()) 
     
-    def add_mute(self, mute_min, user_id):
+    def add_mute(self, user_id, mute_time):
         with self.connection:
-            return self.connection.execute(f"UPDATE 'user' SET 'mute_time' = {int(time.time() + mute_min * 60)} WHERE 'user_id' = {user_id}")
+            return self.connection.execute("UPDATE 'users' SET 'mute_time' = ? WHERE 'user_id' = ?", (int(time.time()) + mute_time, user_id))
         
